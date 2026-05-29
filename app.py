94008e50a93091cd5d4cc03a68b7046c0e1a17b7
Cristian <cristian.urrutia@camir.cl>
2026-05-29 14:49:51 -0400

    Fix render_template sin barra

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def index():
 
 @app.route('/pedidos')
 def pedidos():
-    return render_template('/pedidos')
+    return render_template('pedidos.html')
 
 @app.route('/seguimiento')
 def seguimiento():
